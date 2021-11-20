7d241e70b159cf85e411305024be00f45150928b
Dr Asad Ali <91006488+jankarikiduniya@users.noreply.github.com>
2021-11-20 17:46:09 +0500

    Create youtube.py

---
+++ youtube/youtube.py
@@ -0,0 +1,35 @@
+# A Powerful Music Bot Property Of Rocks Indian Largest Chatting Group
+# Without Credit (Mother Fucker)
+# Rocks © @Dr_Asad_Ali © Rocks
+# Owner Asad + Harshit
+
+
+from os import path
+
+from yt_dlp import YoutubeDL
+
+from config import DURATION_LIMIT
+from helpers.errors import DurationLimitError
+
+
+ydl_opts = {
+    "format": "bestaudio/best",
+    "geo-bypass": True,
+    "nocheckcertificate": True,
+    "outtmpl": "downloads/%(id)s.%(ext)s",
+}
+ydl = YoutubeDL(ydl_opts)
+
+
+def download(url: str) -> str:
+    info = ydl.extract_info(url, False)
+    duration = round(info["duration"] / 60)
+
+    if duration > DURATION_LIMIT:
+        raise DurationLimitError(
+            f"❌ **Vɪᴅᴇᴏs ʟᴏɴɢᴇʀ ᴛʜᴀɴ {DURATION_LIMIT} ᴍɪɴᴜᴛᴇ(s) ᴀʀᴇɴ'ᴛ ᴀʟʟᴏᴡᴇᴅ, ᴛʜᴇ ᴘʀᴏᴠɪᴅᴇᴅ ᴠɪᴅᴇᴏ ɪs {duration} ᴍɪɴᴜᴛᴇ(s)...**"
+        )
+
+    ydl.download([url])
+    return path.join("**Dᴏᴡɴʟᴏᴀᴅ**", f"{info['id']}.{info['ext']}")
+
