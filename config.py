c7de004482a6c0cd088dc9c7c989f5085fbbbeb6
Dr Asad Ali <91006488+jankarikiduniya@users.noreply.github.com>
2021-11-25 22:58:00 +0500

    Update config.py

--- config.py
+++ config.py
@@ -1,33 +1,28 @@
+import os
 from os import getenv
-
 from dotenv import load_dotenv
 
-load_dotenv()
+if os.path.exists("local.env"):
+    load_dotenv("local.env")
 
-que = {}
+load_dotenv()
+admins = {}
 SESSION_NAME = getenv("SESSION_NAME", "session")
 BOT_TOKEN = getenv("BOT_TOKEN")
-BOT_NAME = getenv("BOT_NAME")
-HEROKU_API_KEY = getenv("HEROKU_API_KEY")
-HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
-ASSISTANT_USERNAME = getenv("ASSISTANT_USERNAME")
-UPDATES_CHANNEL = getenv("UPDATES_CHANNEL", "Give_Me_Heart")
-GROUP_SUPPORT = getenv("GROUP_SUPPORT", "Shayri_Music_Lovers")
-BOT_USERNAME = getenv("BOT_USERNAME")
-BG_IMAGE = getenv("BG_IMAGE", "https://telegra.ph/file/f5601720a2c4449823626.jpg")
-THUMB_IMG = getenv("THUMB_IMG", "https://telegra.ph/file/f5601720a2c4449823626.jpg")
-BOT_IMG = getenv("BOT_IMG", "https://telegra.ph/file/f5601720a2c4449823626.jpg")
-AUD_IMG = getenv("AUD_IMG", "https://telegra.ph/file/850757191197c3169c604.jpg")
-QUE_IMG = getenv("QUE_IMG", "https://telegra.ph/file/850757191197c3169c604.jpg")
-
-admins = {}
+BOT_NAME = getenv("BOT_NAME", "Video Stream")
 API_ID = int(getenv("API_ID"))
 API_HASH = getenv("API_HASH")
-
-OWNER_ID = int(getenv("OWNER_ID"))
-
-DURATION_LIMIT = int(getenv("DURATION_LIMIT", "400"))
-
-COMMAND_PREFIXES = list(getenv("COMMAND_PREFIXES", "/ !").split())
-
+OWNER_NAME = getenv("OWNER_NAME", "Dr_Asad_Ali")
+ALIVE_NAME = getenv("ALIVE_NAME", "Dr_Asad_Ali")
+BOT_USERNAME = getenv("BOT_USERNAME", "Asad_Music_Bot")
+ASSISTANT_NAME = getenv("ASSISTANT_NAME", "Music_Asistant")
+GROUP_SUPPORT = getenv("GROUP_SUPPORT", "Shayri_Music_Lovers")
+UPDATES_CHANNEL = getenv("UPDATES_CHANNEL", "AsadSupport")
 SUDO_USERS = list(map(int, getenv("SUDO_USERS").split()))
+COMMAND_PREFIXES = list(getenv("COMMAND_PREFIXES", "/ ! .").split())
+ALIVE_IMG = getenv("ALIVE_IMG", "https://telegra.ph/file/163e51ab720f0085c53a5.jpg")
+DURATION_LIMIT = int(getenv("DURATION_LIMIT", "400"))
+UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/jankarikiduniya/Dr-Music-Video-Streaming")
+IMG_1 = getenv("IMG_1", "https://telegra.ph/file/7169e0ea05e66f0ffd1a2.jpg")
+IMG_2 = getenv("IMG_2", "https://telegra.ph/file/0aec1809fac360415d4cb.jpg")
+IMG_3 = getenv("IMG_3", "https://telegra.ph/file/3234fc59a3c050513de49.jpg")
